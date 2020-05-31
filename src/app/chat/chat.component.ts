5be33c27427263bafec8f96fdeede1e04bd6ab23
Harry Hu <harry.hu@yahoo.ca>
2020-05-31 14:32:29 -0400

    add weui libs

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+// import 'weui';
+import weui from 'weui.js';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    weui.alert('ddd');
+  }
+
+}
