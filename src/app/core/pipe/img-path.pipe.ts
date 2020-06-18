d59413d127f51cea089f7184cb745d2a0301cb67
Harry Hu <harry.hu@yahoo.ca>
2020-06-18 16:34:03 -0400

    fix related path of icons

--- src/app/core/pipe/img-path.pipe.ts
+++ src/app/core/pipe/img-path.pipe.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
 export class ImgPathPipe implements PipeTransform {
 
   transform(value: string): string {
-    if (!value) return '/assets/images/noimage.jpg';
+    if (!value) return 'assets/images/noimage.jpg';
     if (value.indexOf('data:') === 0 || value.match('^http(s)?://')?.length) return value;
     return environment.imageServer + value;
   }
