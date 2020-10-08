9d5df2106cf5838b1b5e5c681bfa6739cae05dfc
Harry Hu <harry.hu@yahoo.ca>
2020-10-07 22:40:10 -0400

    add dev environment

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'api/',
+  // apiUrl: 'http://192.168.87.250:3000/api/',
+  socketUrl: 'http://192.168.87.250:3000',
+  // socketUrl: 'http://timebox.i234.me:3000',
+  imageServer: 'http://timebox.i234.me/images/',
+  wechatServer: 'http://timebox.i234.me/wechat/'
+};
