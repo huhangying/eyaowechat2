1e9ef039d867efe2d0518eb0ab889b3e30fe2c60
Harry Hu <harry.hu@yahoo.ca>
2021-01-09 21:46:43 -0500

    + wx material news models

--- src/app/models/public.model.ts
+++ src/app/models/public.model.ts
@@ -2,3 +2,26 @@ export interface Faq {
   question: string;
   answer: string;
 }
+
+export interface WxMaterial {
+  total_count: number;
+  item_count: number;
+  item: [{
+    media_id: string;
+    content: {
+      news_item: WxMaterialNewsItem[];
+    };
+    update_time: Date;
+  }];
+}
+
+export interface WxMaterialNewsItem {
+  title: string;
+  thumb_media_id: string; // THUMB_MEDIA_ID,
+  show_cover_pic: boolean; // SHOW_COVER_PIC(0 / 1),
+  author: string;
+  digest: string;
+  content: string;
+  url: string;
+  content_source_url: string;
+}
--- src/app/services/public.service.ts
+++ src/app/services/public.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from '../core/services/api.service';
-import { Faq } from '../models/public.model';
+import { Faq, WxMaterial } from '../models/public.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +14,13 @@ export class PublicService {
 
   getFaqs(hid: number): Observable<Faq[]> {
     return this.api.get<Faq[]>('faqs/wechat/auth/' + hid);
+  }
+  
+  getMaterialCount() {
+    return this.api.get<{news_count: number}>('wechat/material-count');
+  }
+
+  getWxMaterialList(page=0) {
+    return this.api.get<WxMaterial>('wechat/material-list/' + page)
   }
 }
