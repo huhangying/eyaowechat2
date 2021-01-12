f3e0ec97bdbd597b39916e261596749103f3631a
Harry Hu <harry.hu@yahoo.ca>
2021-01-11 22:28:44 -0500

    add stage env

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  apiUrl: 'api/',
+  // apiUrl: 'http://192.168.87.250:3000/api/',
+  // socketUrl: 'http://192.168.87.250:3000',
+  socketUrl: 'http://www.zhaoyaoshi885.com:3000',
+  imageServer: 'http://www.zhaoyaoshi885.com/images/',
+  wechatServer: 'http://www.zhaoyaoshi885.com/wechat/',
+  bypassPayment: false,
+};
