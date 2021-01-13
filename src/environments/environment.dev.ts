6cf218ce5e7bbeb802b5a46f994804ec4fdc90c7
Harry Hu <harry.hu@yahoo.ca>
2021-01-13 17:06:02 -0500

    update socket.io to 2.4.0

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,7 +4,7 @@ export const environment = {
   // apiUrl: 'http://192.168.87.250:3000/api/',
   // socketUrl: 'http://timebox.i234.me:3000',
   imageServer: 'http://timebox.i234.me/images/',
-  socketUrl: 'http://timebox.i234.me:3000',
+  socketUrl: 'ws://timebox.i234.me:3000',
   wechatServer: 'http://timebox.i234.me/wechat/',
   bypassPayment: true,
 };
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl: 'api/',
   // apiUrl: 'http://192.168.87.250:3000/api/',
   // socketUrl: 'http://192.168.87.250:3000',
-  socketUrl: 'http://www.zhaoyaoshi885.com:3000',
+  socketUrl: 'ws://www.zhaoyaoshi885.com:3000',
   imageServer: 'http://www.zhaoyaoshi885.com/images/',
   wechatServer: 'http://www.zhaoyaoshi885.com/wechat/',
   bypassPayment: false,
